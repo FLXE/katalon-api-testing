65d40b1a503bba4a9c1ad9d781bbee3a24823aa8
wisamaisam <wisamaisam@gmail.com>
2024-03-10 16:08:35 +0700

    Create Test Cases and Suite template for user API

---
+++ Test Suites/User/User API Smoke test.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>User API Smoke test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>d9dbac00-4532-4316-90be-f748223e8456</testSuiteGuid>
+</TestSuiteEntity>
